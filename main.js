67b498b909786a2ac8917889407ad02bbbd79ace
akshatjain263 <126788590+akshatjain263@users.noreply.github.com>
2023-05-20 03:31:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function render(images){
         link.setAttribute("href",d);
         link.setAttribute("target","_blank")
         link.innerHTML="<br>Metacritic Link";
-        var rating1=document.createElement("pre");
+        var rating1=document.createElement("p");
         var a=element['metacriticScore'];
         var b=element['steamRatingText'];
         var c=element["steamRatingPercent"];
@@ -45,7 +45,7 @@ function render(images){
         link1.setAttribute("target","_blank")
         link1.innerHTML="Steam Link <br><br>";
         price.innerHTML="Normal Price: "+f+"<br> Sale Price: "+e+"<br>"+"Discount: "+g+"%";
-        rating1.innerHTML=id+' Metacritic Score: '+a +'        ' + 'Steam Rating: ' +b + '  ' + c +'%<br><br>' + "Release Date: "+numdate ;
+        rating1.innerHTML=' Metacritic Score: '+a +'<br>'+ 'Steam Rating: ' +b + '  ' + c +'%<br><br>' + "Release Date: "+numdate ;
 
         document.getElementById("grid").appendChild(name);
         document.getElementById("grid").appendChild(image);
